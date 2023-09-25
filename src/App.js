2d7122828474bf98dd0efb4d913576543654b8f1
zakaria janah <zakariajanah3@gmail.com>
2023-09-25 10:02:10 +0100

    comited by zakaria

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const recipient = <span>Imane</span>;
+
+  const achievements = (
+    <ul id="my-achievements">
+      <li className="ln-achievement">Achievement 1</li>
+      <li className="ln-achievement">Achievement 2</li>
+      <li className="ln-achievement">Achievement 3</li>
+    </ul>
+  );
+
+  const getCurrentDate = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    return `${dd}/${mm}/${yyyy}`;
+  };
+
+  const getCurrentName = () => {
+    const currentMinute = new Date().getMinutes();
+    return currentMinute % 2 === 0 ? 'ZAKARIA JANAH' : 'YourLastName';
+  };
+
+  const body = (
+    <div>
+      <h2>Dear {recipient}</h2>
+      <br />
+      <p>
+        Hi, my name is {getCurrentName()}, and I would like to introduce myself to you. I am applying for the position of React developer.
+      </p>
+      <br />
+      <p>I’ve attached my CV to this email.</p>
+      <p>You can find details of my previous roles, experience, and achievements.</p>
+      <p>I’m particularly proud of :</p>
+      {achievements}
+      <p>
+        I’m excited about this opportunity and would love to know more about the process, including timelines for you to decide on interviews.
+      </p>
     </div>
+  );
+
+  const signature = (
+    <div>
+      <br />
+      <span>Best regards,</span>
+      <span>{getCurrentName()}</span>
+      <b>{getCurrentDate()}</b>
+    </div>
+  );
+
+  return (
+    <Container>
+      {body}
+      {signature}
+    </Container>
   );
 }
 
 export default App;
+
